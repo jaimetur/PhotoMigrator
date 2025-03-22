fabad3bef9070e5f9e10a70a972a4bec5e94a616
jaimetur <35789703+jaimetur@users.noreply.github.com>
2025-03-22 11:42:09 +0100

    Update CloudPhotoMigrator.sh
    Signed-off-by: jaimetur <35789703+jaimetur@users.noreply.github.com>

--- docker/CloudPhotoMigrator.sh
+++ docker/CloudPhotoMigrator.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-# Get the current directory
+# Get the current directory (from the user context)
 CURRENT_DIR="$(pwd)"
+
+# Pull the latest version of the image to avoid using a cached one
+docker pull jaimetur/cloudphotomigrator:latest
 
 # Run the Docker container with the current directory mounted to /data
 docker run --rm \
