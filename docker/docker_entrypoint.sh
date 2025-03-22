dd0cca281939bba37b4b022f5ef3600752fc9dd5
jaimetur <35789703+jaimetur@users.noreply.github.com>
2025-03-22 11:12:00 +0100

    Update docker_entrypoint.sh
    Signed-off-by: jaimetur <35789703+jaimetur@users.noreply.github.com>

--- docker/docker_entrypoint.sh
+++ docker/docker_entrypoint.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-CONFIG_FILE="/app/Config.ini"
+CONFIG_FILE="/data/Config.ini"
 DEFAULT_CONFIG="/app/default_config.ini"
 
+# Si no existe el archivo de configuración en la carpeta montada
 if [ ! -f "$CONFIG_FILE" ]; then
-    echo "Config.ini not found in the current directory."
+    echo "Config.ini not found in the current folder."
     echo "Creating a default configuration file..."
     cp "$DEFAULT_CONFIG" "$CONFIG_FILE"
     echo "Please edit Config.ini with your settings and run the script again."
     exit 1
 fi
 
+# Ejecutar el script usando la configuración del volumen montado
 exec python3 /app/src/CloudPhotoMigrator.py "$@"
