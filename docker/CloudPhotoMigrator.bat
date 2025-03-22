97ee20396a1993fe2fa1478d70cc9f3fb5eb3af5
jaimetur <35789703+jaimetur@users.noreply.github.com>
2025-03-22 11:14:42 +0100

    Update CloudPhotoMigrator.bat
    Signed-off-by: jaimetur <35789703+jaimetur@users.noreply.github.com>

--- docker/CloudPhotoMigrator.bat
+++ docker/CloudPhotoMigrator.bat
@@ -1,3 +1,6 @@
 @echo off
-:: Run CloudPhotoMigrator inside Docker using the current directory
-docker run --rm -v "%cd%:/app" -w /app jaimetur/cloudphotomigrator %*
+REM Get the current directory
+set CURRENT_DIR=%cd%
+
+REM Run the Docker container with the current directory mounted to /data
+docker run --rm -v "%CURRENT_DIR%":/data jaimetur/cloudphotomigrator:latest %*
