a48153478489d9d282bfc2c4d7dde21901c5158b
jaimetur <35789703+jaimetur@users.noreply.github.com>
2025-03-22 11:42:24 +0100

    Update CloudPhotoMigrator.bat
    Signed-off-by: jaimetur <35789703+jaimetur@users.noreply.github.com>

--- docker/CloudPhotoMigrator.bat
+++ docker/CloudPhotoMigrator.bat
@@ -2,5 +2,8 @@
 REM Get the current directory
 set CURRENT_DIR=%cd%
 
+REM Pull the latest version of the Docker image to avoid using a cached one
+docker pull jaimetur/cloudphotomigrator:latest
+
 REM Run the Docker container with the current directory mounted to /data
 docker run --rm -v "%CURRENT_DIR%":/data jaimetur/cloudphotomigrator:latest %*
