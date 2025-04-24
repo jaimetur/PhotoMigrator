00f1cb24452b731952ef0d7667e0ba05abcf5267
jaimetur <jaimetur@gmail.com>
2025-04-24 18:32:24 +0200

    Commit from PyCharm (@jaimetur)
    Signed-off-by: jaimetur <jaimetur@gmail.com>

--- src/ClassImmichPhotos.py
+++ src/ClassImmichPhotos.py
@@ -1737,6 +1737,9 @@ class ClassImmichPhotos:
         """
         with set_log_level(LOGGER, log_level):
             self.login(log_level=log_level)
+            LOGGER.info("")
+            LOGGER.info("INFO    : Searching for albums that matches the provided pattern and filters (if applies). This process may take some time. Please be patient!...")
+            LOGGER.info("")
             albums = self.get_albums_owned_by_user(with_filters=True, log_level=log_level)
             if not albums:
                 LOGGER.info("INFO    : No albums found.")
@@ -1783,6 +1786,9 @@ class ClassImmichPhotos:
         """
         with set_log_level(LOGGER, log_level):
             self.login(log_level=log_level)
+            LOGGER.info("")
+            LOGGER.info("INFO    : Searching for albums that matches the provided pattern and filters (if applies). This process may take some time. Please be patient!...")
+            LOGGER.info("")
             albums = self.get_albums_owned_by_user(with_filters=True, log_level=log_level)
             if not albums:
                 LOGGER.info("INFO    : No albums found.")
