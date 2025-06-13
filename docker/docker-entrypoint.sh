45b589d82ae7f61e5a9c91e11e38aec25188bfdf
jaimetur <jaimetur@gmail.com>
2025-06-13 16:25:50 +0200

    Commit from Pycharm
    Signed-off-by: jaimetur <jaimetur@gmail.com>

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,48 +1,32 @@
 #!/bin/bash
 
-CONFIG_DIR="/docker"
-DOCKER_CONF_FILE="$CONFIG_DIR/docker.conf"
+DOCKER_CONF_FILE="/docker/docker.conf"
 DEFAULT_DOCKER_CONF_FILE="/app/default_docker.conf"
 
-CONFIG_FILE="$CONFIG_DIR/Config.ini"
+CONFIG_FILE="/docker/Config.ini"
 DEFAULT_CONFIG="/app/default_config.ini"
 
-# Entrar en modo interactivo si se llama con "bash"
+# To enter in interective shell moode we can call this from Shell:
+# docker run -it --rm -v "${PWD}:/docker" -e TZ=Europe/Madrid jaimetur/photomigrator:latest bash
 if [[ "$1" == "bash" ]]; then
     echo "🔧 Entering interactive shell..."
     exec /bin/bash
 fi
 
-echo "📂 Checking mounted files in /docker:"
-ls -l "$CONFIG_DIR"
-
-# docker.conf
-if [ ! -r "$DOCKER_CONF_FILE" ]; then
-    echo "❌ docker.conf not found or not readable at $DOCKER_CONF_FILE"
-    if [ -w "$CONFIG_DIR" ]; then
-        echo "🛠️  Creating a default docker.conf file..."
-        cp "$DEFAULT_DOCKER_CONF_FILE" "$DOCKER_CONF_FILE"
-    else
-        echo "⚠️  Cannot write to $CONFIG_DIR. Please create docker.conf manually."
-    fi
-else
-    echo "✅ docker.conf found"
+if [ ! -f "$DOCKER_CONF_FILE" ]; then
+    echo "❌ docker.conf not found in the current folder."
+    echo "Creating a default docker.conf file..."
+    cp "$DEFAULT_DOCKER_CONF_FILE" "$DOCKER_CONF_FILE"
 fi
 
-# Config.ini
 echo "🚀 Initializing container and launching PhotoMigrator..."
-if [ ! -r "$CONFIG_FILE" ]; then
-    echo "❌ Config.ini not found or not readable at $CONFIG_FILE"
-    if [ -w "$CONFIG_DIR" ]; then
-        echo "🛠️  Creating a default configuration file..."
-        cp "$DEFAULT_CONFIG" "$CONFIG_FILE"
-        echo "✏️  Please edit Config.ini with your settings and run the script again."
-    else
-        echo "⚠️  Cannot write to $CONFIG_DIR. Please create Config.ini manually."
-    fi
+echo "Looking for: Config.ini"
+if [ ! -f "$CONFIG_FILE" ]; then
+    echo "❌ Config.ini not found in the current folder."
+    echo "Creating a default configuration file..."
+    cp "$DEFAULT_CONFIG" "$CONFIG_FILE"
+    echo "Please edit Config.ini with your settings and run the script again."
     exit 1
-else
-    echo "✅ Config.ini found"
 fi
 
 exec python3 /app/src/PhotoMigrator.py "$@"
